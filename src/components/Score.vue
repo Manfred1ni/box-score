8cc698aee34e9643e0335d1fa5f41af5ff8aff5d
Manfred <cheblimanfred@gmail.com>
2019-06-25 22:49:57 +0200

    wip: try to flatten the scoreboard q

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <v2-table :data="scoreboard">
+    <v2-table :data="flattenedScoreboard">
       <v2-table-column label="Teams" prop="team"></v2-table-column>
       <v2-table-column
-        v-for="inning in 9"
+        v-for="inning in innings"
         :key="inning"
-        :label="inning.toString()"
-        :prop="`score[${inning}]`"
+        label="inning"
+        :prop="inning"
       ></v2-table-column>
       <v2-table-column label="R" prop="totalRuns"></v2-table-column>
       <v2-table-column label="H" prop="hits"></v2-table-column>
@@ -18,6 +18,24 @@
 export default {
   props: {
     scoreboard: Array
+  },
+  computed: {
+    flattenedScoreboard: () => {
+      /* eslint-disable */
+      const flatScoreboard = [];
+      this.scoreboard.map((score) => {
+        const newScore = {
+          team: score.team,
+          hits: score.hits,
+          errors: score.errors,
+          innings: Object.keys(score.scores).length
+        }
+        score.scores.forEach((inningResult, index) => {
+          newScore[index+1] = inningResult
+        });
+        flatScoreboard.push(newScore);
+      })
+    }
   }
 };
 </script>
